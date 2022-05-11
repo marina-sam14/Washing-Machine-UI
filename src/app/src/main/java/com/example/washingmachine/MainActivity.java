6249e234d6f8fa791b1cd3c40975b4c34b7f0f50
marina-sam <marina.samb14@gmail.com>
2022-05-11 14:04:22 +0300

    "Add Settings xml"

--- src/app/src/main/java/com/example/washingmachine/MainActivity.java
+++ src/app/src/main/java/com/example/washingmachine/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,Settings.class);
+                startActivity(intent);
             }
         });
 
