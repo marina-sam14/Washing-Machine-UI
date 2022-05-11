37db7855ba6d348b1e567d20bb530fd545bb3817
marina-sam <marina.samb14@gmail.com>
2022-05-11 14:31:23 +0300

    Fixed Home Page

---
+++ src/app/src/main/java/com/example/washingmachine/Settings.java
@@ -0,0 +1,15 @@
+package com.example.washingmachine;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_first);
+
+
+    }
+}
