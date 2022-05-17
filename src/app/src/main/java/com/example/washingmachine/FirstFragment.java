acd6d8481b87c88aa0b842a26258d8862d7fda5a
marina-sam <marina.samb14@gmail.com>
2022-05-17 12:25:38 +0300

    Added some checks in Program Selection, Starting progress bar

--- src/app/src/main/java/com/example/washingmachine/FirstFragment.java
+++ src/app/src/main/java/com/example/washingmachine/FirstFragment.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.washingmachine.databinding.FragmentFirstBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FirstFragment extends AppCompatActivity {
 
@@ -29,6 +32,7 @@ public class FirstFragment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_first);
 
+
         Spinner spinnerLanguages = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
@@ -36,17 +40,104 @@ public class FirstFragment extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerLanguages.setAdapter(adapter);
 
+        CheckBox progrBox1 = (CheckBox) findViewById(R.id.normalcheckBox6);
+        CheckBox progrBox2 = (CheckBox) findViewById(R.id.normalcheckBox7);
+        CheckBox progrBox3 = (CheckBox) findViewById(R.id.normalcheckBox5);
+        progrBox1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                // Check which checkbox was clicked
+                if (checked){
+                    progrBox2.setEnabled(false);
+                    progrBox3.setEnabled(false);
+//                    Log.i("mytag","ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ");
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ ΔΙΑΡΚΕΙΑΣ 15' ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                if (!checked){
+                    progrBox2.setEnabled(true);
+                    progrBox3.setEnabled(true);
+//                    Log.i("mytag","ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ");
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ ΔΙΑΡΚΕΙΑΣ 15' ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
+        progrBox2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                // Check which checkbox was clicked
+                if (checked){
+//                    Log.i("mytag","ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ");
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΕΛΑΦΡΥ ΠΡΟΓΡΑΜΜΑ ΔΙΑΡΚΕΙΑΣ 60' ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
+        progrBox3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                // Check which checkbox was clicked
+                if (checked){
+//                    Log.i("mytag","ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΚΑΝΟΝΙΚΟ ΠΡΟΓΡΑΜΜΑ");
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΙΣΧΥΡΟ ΠΡΟΓΡΑΜΜΑ ΔΙΑΡΚΕΙΑΣ 60' ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
+        CheckBox extra1 = (CheckBox) findViewById(R.id.normalcheckBox8);
+        CheckBox extra2 = (CheckBox) findViewById(R.id.normalcheckBox9);
+        extra1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                // Check which checkbox was clicked
+                if (checked){
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΣΤΥΨΙΜΟ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
+        extra2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                // Check which checkbox was clicked
+                if (checked){
+                    Snackbar.make(v, "ΕΧΕΤΕ ΕΠΙΛΕΞΕΙ ΞΕΒΓΑΛΜΑ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+            }
+        });
+
         ImageButton start = (ImageButton) findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(FirstFragment.this,Laundring.class);
-                startActivity(intent);
+                if (!progrBox1.isChecked() && !progrBox2.isChecked() && !progrBox3.isChecked()){
+                    Snackbar.make(view, "ΠΡΕΠΕΙ ΝΑ ΕΠΙΛΕΞΕΤΕ ΠΡΟΓΡΑΜΜΑ ΓΙΑ ΝΑ ΞΕΚΙΝΗΣΕΤΕ", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+                else {
+//                    an kanoume diaforetika gia kathe thermokrasia, edw if-else
+                    Intent intent = new Intent(FirstFragment.this, Laundring.class);
+                    startActivity(intent);
+                }
             }
         });
-
-        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_first, container, false);
 
 
     }
---
+++ src/app/src/main/java/com/example/washingmachine/Laundring.java
@@ -0,0 +1,56 @@
+package com.example.washingmachine;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Laundring extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.laundrying);
+
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        Drawable progressDrawable = progressBar.getIndeterminateDrawable().mutate();
+        progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBar.setProgressDrawable(progressDrawable);
+        progressBar.setIndeterminate(true);
+
+        progressBar.setProgressTintList((ColorStateList.valueOf(Color.RED)));
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, progressBar.getMax());
+        animator.setDuration(3000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation){
+                progressBar.setProgress((Integer)animation.getAnimatedValue());
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // start your activity here
+            }
+        });
+        animator.start();
+
+        TextView header = (TextView) findViewById(R.id.textView4);
+
+    }
+
+
+
+}
