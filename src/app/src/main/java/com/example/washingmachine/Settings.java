2b85617408e98d87b4f9b24f315b1df2d74bf938
marina-sam <marina.samb14@gmail.com>
2022-05-26 21:35:58 +0300

    Settings done

--- src/app/src/main/java/com/example/washingmachine/Settings.java
+++ src/app/src/main/java/com/example/washingmachine/Settings.java
@@ -1,11 +1,20 @@
 package com.example.washingmachine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,26 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        TextView date = findViewById(R.id.date);
+        TextView clock = findViewById(R.id.clock);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String dateTime = simpleDateFormat.format(calendar.getTime()).toString();
+        date.setText(dateTime);
+
+        String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+        clock.setText(currentTime);
+
+        ImageButton home = (ImageButton) findViewById(R.id.homepage);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
