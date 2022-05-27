19bb01b4c69205dd8f9206b13c439acff6ca0699
marina-sam <marina.samb14@gmail.com>
2022-05-27 16:10:47 +0300

    Fixed date in completed xml

--- AndroidApp/app/src/main/java/com/example/washingmachine/Completed.java
+++ AndroidApp/app/src/main/java/com/example/washingmachine/Completed.java
@@ -7,10 +7,16 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Completed extends AppCompatActivity {
 
@@ -22,6 +28,17 @@ public class Completed extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.completed);
+
+        TextView date = findViewById(R.id.date);
+        TextView clock = findViewById(R.id.clock);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String dateTime = simpleDateFormat.format(calendar.getTime()).toString();
+        date.setText(dateTime);
+
+        String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+        clock.setText(currentTime);
 
         voiceOn = getIntent().getBooleanExtra("VOICE_ON", false);
 
