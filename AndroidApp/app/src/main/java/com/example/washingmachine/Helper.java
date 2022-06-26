dd747d9d6100ea6ada6abea724f2e24c8c267b3f
marina-sam <marina.samb14@gmail.com>
2022-06-26 13:22:59 +0300

    Fixed Helper voice

--- AndroidApp/app/src/main/java/com/example/washingmachine/Helper.java
+++ AndroidApp/app/src/main/java/com/example/washingmachine/Helper.java
@@ -1,6 +1,7 @@
 package com.example.washingmachine;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Helper extends AppCompatActivity {
+    boolean voiceOn;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,13 +23,19 @@ public class Helper extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.helper);
 
+        voiceOn = getIntent().getBooleanExtra("VOICE_ON", false);
         ImageButton exit = (ImageButton) findViewById(R.id.exit);
 
 //        X goes back to Program Choice menu
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (voiceOn) {
+                    MediaPlayer music = MediaPlayer.create(Helper.this, R.raw.select_program);
+                    music.start();
+                }
                 Intent intent = new Intent(Helper.this, FirstFragment.class);
+                intent.putExtra("VOICE_ON", voiceOn);
                 startActivity(intent);
             }
         });
